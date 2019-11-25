ce0c324318624ad94f78fda73f2fb21d3af807c1
brandonezzedine <36865335+brandonezzedine@users.noreply.github.com>
2019-11-25 18:09:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿const admin = require('./node_modules/firebase-admin');
+const serviceAccount = require("./serviceAccountKey.json");
+const data = require("./staff.json");
+const key = "f3159ac1f824fdf3b29da0b051696b2be425eaf2";
+const collectionKey = "ServiceProvider/10001/services"; //name of the collection
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sweng-581-capstone.firebaseio.com/"
+});
+const firestore = admin.firestore();
+const settings = { timestampsInSnapshots: true };
+console.log(data)
+firestore.settings(settings);
+if (data && (typeof data === "object")) {
+Object.keys(data).forEach(docKey => {
+ firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+    console.log("Document " + docKey + " successfully written!");
+}).catch((error) => {
+   console.error("Error writing document: ", error);
+});
+});
+}
